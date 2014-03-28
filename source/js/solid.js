bebdd323db6137dc7e9ff73f065b203ac84cad6e
Ryo Nakae <me@ryonakae.com>
2014-03-28 17:45:18 +0900

    Google Code Prettify調整

--- source/js/solid.js
+++ source/js/solid.js
@@ -1,33 +1,34 @@
 jQuery(function(){
   // google-code-prettify
   jQuery("pre").addClass("prettyprint");
-  
-  
+  prettyPrint();
+
+
   // fitvids
   jQuery("iframe[src*='youtube.com']").wrap('<div class="video_wrapper"></div>');
   jQuery(".video_wrapper").fitVids();
-  
-  
+
+
   // Menu
   function toggleMenu() {
     var nav = jQuery("#nav");
     var toggle = jQuery("#nav-toggle");
     var wrapper = jQuery("#wrapper");
-    
+
     var navHeight = nav.outerHeight();
     jQuery(window).on("resize", function(){
       var navHeight = nav.outerHeight();
     });
-    
+
     nav.css({
       "top" : navHeight * -1,
       "opacity" : 1
     });
-    
+
     var flag = "close";
     var speed = 600;
     var easing = "easeInOutCubic";
-    
+
     function navOpen() {
       nav.stop().transition({ "top" : 0 }, speed, easing);
       wrapper.stop().transition({ "top" : navHeight, delay : 50 }, speed, easing);
@@ -40,7 +41,7 @@ jQuery(function(){
       flag = "close";
       toggle.removeClass("navopen");
     }
-    
+
     toggle.on("click", function(){
       if (flag === "close") {
         navOpen();
@@ -49,16 +50,16 @@ jQuery(function(){
         navClose();
       }
     });
-    
+
     jQuery(window).on("resize", function(){
       navClose();
     });
   };
   toggleMenu();
-  
-  
+
+
 	// Back to Top
-	function backTop() {	
+	function backTop() {
 		jQuery("#backtop-arrow").on("click", function(){
 			jQuery("html, body").stop().animate({ scrollTop : 0 }, 1000, "easeOutCubic");
 		});
